9a5268e44bb7771d0c4baaaa258a74a72a2b3fc0
Julio <juhume.exe@gmail.com>
2019-10-08 19:03:13 +0200

    first commit

---
+++ front/src/components/contents/UserHome.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./UserHome.css";
+
+//clase componente que renderiza los contenidos genéricos
+//usando rendering condicional y el componente Switch que ya conocéis podéis mostrar los contenidos oportunos que queráis
+class UserHome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>All Haikus</h1>
+      </div>
+    );
+  }
+}
+
+export default UserHome;
